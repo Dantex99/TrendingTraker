ae5cd07fc620f215f38063eb5519db49d01c4055
Dpalacios <60647444+Dpalacios@users.noreply.github.com>
2020-05-05 11:44:34 +0200

    Initial Commit
    Primer commit con el estado actual del proyecto
    - Lista de los 10 TT
    - Seguimiento en directo por idioma

---
+++ TrendingTraker/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Tweetinvi;
+
+namespace TrendingTraker
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Auth.SetUserCredentials(
+                "9F948nTxp7DLQlJkNHgC9Zct0",
+                "taJoKFzKIaEdxk4wEUdZSZZmHD9FZ5pB9SP9wlHATY0qq37NEp",
+                "1235663240224559104-3pZyrD0ZVyiK1lTd5OoONBA979nE3e",
+                "hw6OuidUsyT9fSdoLdoxtGt3rZdj2OrUOGB7gMLVBCyrD"
+                );
+            setTrendigns();
+
+        }
+
+        private void setTrendigns()
+        {
+            //Bucle recorriendo los objetos
+            int i = 0;
+            var trends = Trends.GetTrendsAt(1);
+
+            foreach (TextBlock dr in grd_TL.Children)
+            {
+                dr.Text = (i + 1) + "º " + trends.Trends.ToList()[i].Name;
+                i++;
+            }
+
+            //Texto del label = i+"º #"+Trends.GetTrendsAt(i);
+        }
+
+        private void lbl_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock clicked = (TextBlock)sender;
+            String pos = clicked.Text.Substring(3);
+            StatisticsWindow statistics = new StatisticsWindow(pos);
+            statistics.Show();
+            this.Close();
+        }
+    }
+}
---
+++ TrendingTraker/StatisticsWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Tweetinvi;
+using System.Threading;
+using Tweetinvi.Parameters;
+using Tweetinvi.Models;
+
+namespace TrendingTraker
+{
+    /// <summary>
+    /// Lógica de interacción para Window1.xaml
+    /// </summary>
+    public partial class StatisticsWindow : Window
+    {
+        //String de busqueda
+        String obj;
+
+        //Fecha hace una semana
+        DateTime date;
+
+        public StatisticsWindow(String obj)
+        {
+            InitializeComponent();
+            Auth.SetUserCredentials(
+                "9F948nTxp7DLQlJkNHgC9Zct0",
+                "taJoKFzKIaEdxk4wEUdZSZZmHD9FZ5pB9SP9wlHATY0qq37NEp",
+                "1235663240224559104-3pZyrD0ZVyiK1lTd5OoONBA979nE3e",
+                "hw6OuidUsyT9fSdoLdoxtGt3rZdj2OrUOGB7gMLVBCyrD"
+                );
+            this.obj = obj;
+            lbl_SelectTt.Content = obj;
+
+            //Stream
+            Thread hiloStream = new Thread(() => TweetTraker());
+
+            hiloStream.Start();
+
+        }
+
+        //Actividad en vivo del #
+        #region Live
+        private void TweetTraker()
+        {
+
+            int count = 0;
+            int en = 0;
+            int es = 0;
+            int fr = 0;
+
+            var stream = Stream.CreateFilteredStream();
+            stream.AddTrack(obj);
+            stream.MatchingTweetReceived += (sender, args) =>
+            {
+                count++;
+
+                this.Dispatcher.Invoke(() =>
+                {
+
+                    switch (args.Tweet.Language.ToString())
+                    {
+                        case "English":
+                            en++;
+                            lbl_InglesEn.Content = "Tweets en ingles: " + en;
+                            break;
+
+                        case "Spanish":
+                            es++;
+                            lbl_EspanolEn.Content = "Tweets en español: " + es;
+                            break;
+
+                        case "French":
+                            fr++;
+                            lbl_FrancesEn.Content = "Tweets en frances: " + fr;
+                            break;
+                    }
+
+                
+
+                    lbl_Tweets.Content = "Tweets: " + count;
+
+                });
+
+
+            };
+
+            stream.StartStreamMatchingAllConditions();
+
+        }
+
+        #endregion
+       
+    }
+}
